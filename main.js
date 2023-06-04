27a08fbe8f746723d379cee8caa0d861432b04ec
Michelle Appiah <michappiah67@gmail.com>
2023-06-04 13:37:24 +0000

    libraryShelf and bookCard

--- main.js
+++ main.js
@@ -8,3 +8,54 @@ newBookButton.addEventListener("click",function(){
     popUp.style.display = 'none';
    }
 });
+
+function Book(title, author, pages, read) {
+   this.title = title;
+   this.author = author;
+   this.pages = pages;
+   this.read = read;
+}
+
+let myLibrary = [];
+
+function addBookToLibrary(title,author,pages,read) {
+   let newBook = new Book (title, author, pages, read);
+   myLibrary.push(newBook);
+}
+
+function displayBooks() {
+   let libraryShelf = document.querySelector("#library_shelf");
+   libraryShelf.innerHTML = "";
+
+   for(let i = 0; i < myLibrary.length; i++){
+      let bookCard = document.createElement('div');
+      bookCard.idList.add('book_card');
+
+      let bookTitle = document.createElement('h3');
+      bookTitle.textContent = 'Title: ${myLibrary[i].title}';
+
+      let bookAuthor = document.createElement('p');
+      bookAuthor.textContent = 'Author: ${myLibrary[i].author}';
+
+      let bookPages = document.createElement('p');
+      bookPages.textContent = 'Pages: ${myLibrary[i].pages}';
+
+      let readButton = document.createElement('button');
+      readButton.idList.add('read_button');
+      readButton.textContent = 'Read Status';
+
+      let removeButton = document.createElement('button');
+      removeButton.idList.add('remove_button');
+      removeButton.textContent = 'Remove';
+
+
+      bookCard.appendChild(bookTitle);
+      bookCard.appendChild(bookAuthor);
+      bookCard.appendChild(bookPages);
+      bookCard.appendChild(bookRead);
+      bookCard.appendChild(readButton);
+      bookCard.appendChild(removeButton);
+
+      libraryShelf.appendChild(bookCard);
+   }
+}
